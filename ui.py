705f00363119b1b135cb252780a2c6f6bbd4ef02
BihackerViktor <bihacker.viktor@gmail.com>
2019-02-18 15:48:18 +0100

    comment ui

--- ui.py
+++ ui.py
@@ -1,72 +1,63 @@
 # User Interface (UI) module
-    
-    """
-    Prints table with data.
-
-    Example:
-        /-----------------------------------\
-        |   id   |      title     |  type   |
-        |--------|----------------|---------|
-        |   0    | Counter strike |    fps  |
-        |--------|----------------|---------|
-        |   1    |       fo       |    fps  |
-        \-----------------------------------/
-
-    Args:
-        table (list): list of lists - table to display
-        title_list (list): list containing table headers
-
-    Returns:
-        None: This function doesn't return anything it only prints to console.
-    """
+# Prints table with data.
+#    Example:
+#        /-----------------------------------\
+#        |   id   |      title     |  type   |
+#        |--------|----------------|---------|
+#        |   0    | Counter strike |    fps  |
+#        |--------|----------------|---------|
+#        |   1    |       fo       |    fps  |
+#        \-----------------------------------/
+#    Args:
+#        table (list): list of lists - table to display
+#        title_list (list): list containing table headers
+#    Returns:
+#    None: This function doesn't return anything it only prints to console.
+ 
  
 def print_table(table, title_list):
     print('/-----------------------------------\')
-for item in title_list:
-    for j in item:
-        print('|',{item[j]},' '*(20-len(item[j])),'|'))
-    print('\n')
-    print('\-----------------------------------/')
-
+    for item in title_list:
+        for j in item:
+            print('|',{item[j]},' '*(20-len(item[j])),'|'))
+        print('\n')
+        print('\-----------------------------------/')
 
 
 def print_result(result, label):
-    """
-    Displays results of the special functions.
-
-    Args:
-        result: result of the special function (string, list or dict)
-        label (str): label of the result
-
-    Returns:
-        None: This function doesn't return anything it only prints to console.
-    """
-
-    # your code
+#
+#    Displays results of the special functions.
+#
+#    Args:
+#        result: result of the special function (string, list or dict)
+#        label (str): label of the result
+#
+#    Returns:
+#        None: This function doesn't return anything it only prints to console.
+#    """
+#
+#     your code
 
 
 def print_menu(title, list_options, exit_message):
-    """
-    Displays a menu. Sample output:
-        Main menu:
-            (1) Store manager
-            (2) Human resources manager
-            (3) Inventory manager
-            (4) Accounting manager
-            (5) Sales manager
-            (6) Customer relationship management (CRM)
-            (0) Exit program
-
-    Args:
-        title (str): menu title
-        list_options (list): list of strings - options that will be shown in menu
-        exit_message (str): the last option with (0) (example: "Back to main menu")
-
-    Returns:
-        None: This function doesn't return anything it only prints to console.
-    """
-
-    # your code
+#    Displays a menu. Sample output:
+#        Main menu:
+#            (1) Store manager
+#            (2) Human resources manager
+#            (3) Inventory manager
+#            (4) Accounting manager
+#            (5) Sales manager
+#            (6) Customer relationship management (CRM)
+#            (0) Exit program
+#
+#    Args:
+#        title (str): menu title
+#        list_options (list): list of strings - options that will be shown in menu
+#        exit_message (str): the last option with (0) (example: "Back to main menu")
+#
+#    Returns:
+#        None: This function doesn't return anything it only prints to console.
+# your code
     print(title)
     for item, item_no in enumerate(list_options):
         print('\t(' + str(item_no) + ') ' + item)
@@ -74,24 +65,21 @@ def print_menu(title, list_options, exit_message):
 
 
 def get_inputs(list_labels, title):
-    """
-    Gets list of inputs from the user.
-    Sample call:
-        get_inputs(["Name","Surname","Age"],"Please provide your personal information")
-    Sample display:
-        Please provide your personal information
-        Name <user_input_1>
-        Surname <user_input_2>
-        Age <user_input_3>
-
-    Args:
-        list_labels (list): labels of inputs
-        title (string): title of the "input section"
-
-    Returns:
-        list: List of data given by the user. Sample return:
-            [<user_input_1>, <user_input_2>, <user_input_3>]
-    """
+#    Gets list of inputs from the user.
+#    Sample call:
+#        get_inputs(["Name","Surname","Age"],"Please provide your personal information")
+#    Sample display:
+#        Please provide your personal information
+#        Name <user_input_1>
+#        Surname <user_input_2>
+#        Age <user_input_3>
+#
+#    Args:
+#        list_labels (list): labels of inputs
+#        title (string): title of the "input section"
+#    Returns:
+#        list: List of data given by the user. Sample return:
+#            [<user_input_1>, <user_input_2>, <user_input_3>]
     inputs = []
     print(title)
     for labels in list_labels:
@@ -102,14 +90,12 @@ def get_inputs(list_labels, title):
 
 
 def print_error_message(message):
-    """
-    Displays an error message (example: ``Error: @message``)
-
-    Args:
-        message (str): error message to be displayed
-
-    Returns:
-        None: This function doesn't return anything it only prints to console.
-    """
-
-    # your code
+#    Displays an error message (example: ``Error: @message``)
+#
+#    Args:
+#        message (str): error message to be displayed
+#
+#    Returns:
+#        None: This function doesn't return anything it only prints to console.
+#
+#     your code
